70b727f5ea13b9415cfe12ec9307eb273b5915fa
Evyatar Daud <evyatar.daud@gmail.com>
2020-11-08 18:29:36 +0200

    Add auto-complete keyboard navigation

--- src/components/AutoComplete/AutoComplete.js
+++ src/components/AutoComplete/AutoComplete.js
@@ -8,6 +8,7 @@ import './AutoComplete.css';
 function AutoComplete()
 {
     const [ value, setValue ] = useState( '' );
+    const [ currentItem, setCurrentItem ] = useState( -1 );
     const [ loading, setLoading ] = useState( false );
     const dispatch = useDispatch();
     const suggestions = useSelector( state => state.autocomplete );
@@ -48,16 +49,58 @@ function AutoComplete()
     // handle suggestion selection
     const selectSuggesstion = ( i ) => {
 
-        const selected = suggestions[i];
-        console.log( selected );
+        const idx = i ?? currentItem;
+        const selected = suggestions[idx];
 
+        console.log( selected );
+        
+        setCurrentItem( -1 );
         dispatch( setAutoComplete( [] ) );
 
     };
 
 
+    // handle autocomplete keyboard navigation
+    const keyUp = ( e ) => {
+        
+        const keyCode = e.keyCode || e.which;
+
+        switch( keyCode )
+        {
+            // up arrow -> previous item
+            case 38:
+                if( currentItem > 0 )
+                    setCurrentItem( prev => prev - 1 );
+
+                else
+                    setCurrentItem( suggestions.length - 1 );
+
+                break;
+
+
+            // down arrow -> next item
+            case 40:
+                if( currentItem < suggestions.length -1 )
+                    setCurrentItem( prev => prev + 1 );
+                else
+                    setCurrentItem( 0 );
+                break;
+
+
+            // enter -> select item
+            case 13:
+                if( currentItem !== -1 )
+                    selectSuggesstion();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
     return (
-        <form className="auto-complete-form">
+        <form className="auto-complete-form" onSubmit={ e => e.preventDefault() }>
 
             { loading &&
                 <span className="auto-complete-loader"></span>
@@ -68,14 +111,21 @@ function AutoComplete()
                 placeholder="Type to search..."
                 className="auto-complete-input"
                 onChange={ e => setValue( e.target.value) }
+                onKeyUp={ keyUp }
             />
 
             <div className="auto-complete-suggestions">
                 <ul>
                     {
-                        suggestions.slice( 0, 5 ).map(( s, i ) => {;
+                        suggestions.map(( s, i ) => {;
 
-                            return <li onClick={ () => selectSuggesstion( i ) } key={ s.Key }>{ s.LocalizedName }</li>
+                            return (
+                                <li key={ s.Key } 
+                                    onClick={ () => selectSuggesstion( i ) }
+                                    className={ currentItem === i ? 'active' : '' }>
+                                    { s.LocalizedName }
+                                </li>
+                            )
 
                         })
                     }
--- src/redux/autocomplete/reducer.js
+++ src/redux/autocomplete/reducer.js
@@ -9,7 +9,7 @@ const reducer = ( state = defaultState, { type, payload } ) => {
     switch( type )
     {
         case ACTIONS.SET:
-            return payload.autocomplete;
+            return payload.autocomplete.slice( 0, 5 );
 
         default:
             return state;
