5a35fa5804da91f1857f7a57489a77150353ca15
Evyatar Daud <evyatar.daud@gmail.com>
2020-11-09 12:49:09 +0200

    Add LocationKey to the auto complete suggestions

--- src/redux/autocomplete/reducer.js
+++ src/redux/autocomplete/reducer.js
@@ -9,12 +9,27 @@ const reducer = ( state = defaultState, { type, payload } ) => {
     switch( type )
     {
         case ACTIONS.SET:
-            return payload.autocomplete.slice( 0, 5 );
+            return setAutoComplete( payload.autocomplete )
 
         default:
             return state;
     }
 
 };
+
+/*
+    Helpers
+*/
+const setAutoComplete = ( autocomplete ) => {
+
+    let tmp = autocomplete.slice( 0, 5 );
+
+    // add LocationKey to the location object because the API response keys are not consistent
+    tmp.map( ( location ) => location.LocationKey = location.Key );
+
+    return tmp;
+
+}
+
 
 export default reducer;
