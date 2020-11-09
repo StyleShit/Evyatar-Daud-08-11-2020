ab9e95926da4ac6da3b5d4de07d30646fa5221bd
Evyatar Daud <evyatar.daud@gmail.com>
2020-11-09 14:21:32 +0200

    Move default weather loading to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from "react-router-dom";
 import withStore from './HOCs/withStore';
 import withRouter from './HOCs/withRouter';
 import { compose } from 'redux';
+import { useDispatch } from 'react-redux';
 import { Header } from './components/Header';
 import { Favorites, Home } from './pages';
+import { apiCurrentWeather, apiForecast } from './redux/middlewares/api';
 
 import './css/App.css';
 
 
 function App()
 {
+	const dispatch = useDispatch();
+
+	useEffect( () => {
+
+		// fetch default location data on load
+		const defaultLocation = { 
+			LocationKey: '215854',
+			LocalizedName: 'Tel Aviv'
+		};
+
+		dispatch( apiCurrentWeather( defaultLocation ) );
+		dispatch( apiForecast( defaultLocation ) );
+
+	// eslint-disable-next-line
+	}, []);
+
 	return (
 		<div className="app-container">
 			<Header />
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
-import { apiCurrentWeather, apiForecast, apiMiddleware } from './middlewares/api';
+import { apiMiddleware } from './middlewares/api';
 import { loggerMiddleware } from './middlewares/logger';
 import rootReducer from './rootReducer';
 
@@ -16,16 +16,5 @@ if( process.env.NODE_ENV === 'development' )
 
 // export the Redux store with the root reducer & middlewares
 const store = createStore( rootReducer, applyMiddleware( ...middlewares ) );
-
-
-// fetch default location data on load
-const defaultLocation = { 
-    LocationKey: '215854',
-    LocalizedName: 'Tel Aviv'
-};
-
-store.dispatch( apiCurrentWeather( defaultLocation ) );
-store.dispatch( apiForecast( defaultLocation ) );
-
 
 export default store;
