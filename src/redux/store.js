eb555a9b358cf1819a91c58d06aaf069be53c1ee
Evyatar Daud <evyatar.daud@gmail.com>
2020-11-10 15:19:02 +0200

    Add Redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,22 @@ import { applyMiddleware, createStore } from 'redux';
 import { apiMiddleware } from './middlewares/api';
 import { loggerMiddleware } from './middlewares/logger';
 import rootReducer from './rootReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+// configure & initialize persist reducer
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: [
+        'favorites',
+        'preferences'
+    ]
+};
+
+const pReducer = persistReducer( persistConfig, rootReducer );
+
 
 // set store middlewares
 let middlewares = [ 
@@ -14,7 +30,8 @@ if( process.env.NODE_ENV === 'development' )
     middlewares.push( loggerMiddleware );
 }
 
-// export the Redux store with the root reducer & middlewares
-const store = createStore( rootReducer, applyMiddleware( ...middlewares ) );
+
+const store = createStore( pReducer, applyMiddleware( ...middlewares ) );
+export const persistor = persistStore( store );
 
 export default store;
